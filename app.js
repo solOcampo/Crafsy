00266b313e38bc8bb4d8aa1ffc6e3b7241a8b1a3
solOcampo <soolangeeliizabet@gmail.com>
2022-07-06 11:30:59 -0300

    estilos de header mobile terminado

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const express=require('express')
-const path=require ('path')
-const app=express()
-const port=3015
+/* Livereload */
+const livereload = require('livereload');
+const liveReloadServer = livereload.createServer();
 
+/* Entry point */
+const express = require('express')
+const connectLivereload = require('connect-livereload')
+const path = require('path')
+
+const app = express()
+const port = 3015
+
+/* Archivos estaticos */
 app.use(express.static(path.resolve(__dirname,'public')))
-app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'views','index.html')))
-app.get('/login',(req,res)=>res.sendFile(path.resolve(__dirname,'views','login.html')))
-app.get('/register',(req,res)=>res.sendFile(path.resolve(__dirname,'views','register.html')))
 
+/* Archivos estaticos monitoreados */
+liveReloadServer.watch(path.join(__dirname, 'public'));
+app.use(connectLivereload());
+
+/* Rutas */
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html')))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
+
+/* Funcion de actualizacion del servidor */
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+
+/* Levantamos el servidor con app listen */
 app.listen(port,function(){
-    return console.log(`Servidor levantado en http://localhost:${port}`);
-
+    return console.log(`Se levanta el servidor en http://localhost:${port}`)
 })
