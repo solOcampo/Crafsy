b6f9eb279c3da0e67fa66d3d97f37d08ba4d876a
solOcampo <soolangeeliizabet@gmail.com>
2022-07-21 00:04:47 -0300

    subo cambios del carrito

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html
 app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
 app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
 app.get('/detail',(req,res) => res.sendFile(path.resolve(__dirname,'views','detail.html')))
+app.get('/cart',(req,res) => res.sendFile(path.resolve(__dirname,'views','cart.html')))
 
 /* Funcion de actualizacion del servidor */
 liveReloadServer.server.once("connection", () => {
